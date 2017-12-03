d52cbfa6246ab77905c7730500730b4daacffaf7
Bharathkumar <bhrth.dsra1@gmail.com>
2017-12-03 22:16:15 +0530

    Added the s3 bucket details

--- sync_website.sh
+++ sync_website.sh
@@ -1,1 +1,1 @@
-aws s3 sync ~/Work/tahaderouiche.com/ s3://tahaderouiche.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --delete --profile personal-web-user
+aws s3 sync ~/bkweb/bharathkumarraju.com/ s3://bharathkumarraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --delete 
