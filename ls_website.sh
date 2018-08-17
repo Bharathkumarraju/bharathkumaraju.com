5e2e17c4ffedc25241ae234d6d7b5e68e959704a
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-08-17 18:20:13 +0200

    s3 sync

--- ls_website.sh
+++ ls_website.sh
@@ -1,1 +1,1 @@
-aws s3 ls s3://bharathkumaraju.com 
+aws s3 ls s3://bharathkumaraju.com  --profile bharathraju
--- sync_website.sh
+++ sync_website.sh
@@ -1,1 +1,1 @@
-aws s3 sync ~/bkansible/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --delete 
+aws s3 sync  ~/bkansible/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju 
