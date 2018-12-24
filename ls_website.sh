554b753c6148c0131129c71f2ff18c6decec410c
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-12-24 12:12:48 +0100

    Add the shebang line

--- ls_website.sh
+++ ls_website.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 aws s3 ls s3://bharathkumaraju.com  --profile bharathraju
--- sync_website.sh
+++ sync_website.sh
@@ -1,1 +1,2 @@
-aws s3 sync  ~/bkansible/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju 
+#!/usr/bin/env bash
+aws s3 sync  ~/bkansible/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
