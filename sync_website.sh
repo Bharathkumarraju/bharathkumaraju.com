d0b158456bb8023f2c5a9329f517705dbd8c8586
Bharathkumar Dasaraju <bharath.dasaraju@delivion.de>
2018-12-25 11:05:45 +0100

    uodate the index.html

--- sync_website.sh
+++ sync_website.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-aws s3 sync  ~/bkansible/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
+aws s3 sync  ~/Downloads/bharathsweb/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
