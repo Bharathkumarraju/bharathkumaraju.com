73ecf6b3fb7d8531bbafe6b3a69af220f4c9ed31
Bharath Dasaraju <bharath@hooq.tv>
2019-08-19 09:40:16 +0800

    Update latest job

--- sync_website.sh
+++ sync_website.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-aws s3 sync  ~/git/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
-aws s3 sync  ~/git/bharathkumarraju.com/ s3://www.bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
+aws s3 sync  ~/external/bharathkumarraju.com/ s3://bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
+aws s3 sync  ~/external/bharathkumarraju.com/ s3://www.bharathkumaraju.com --exclude "*.sh" --exclude ".DS_Store" --exclude "*.md" --exclude "README" --exclude ".git/*" --exclude "LICENSE" --profile bharathraju
